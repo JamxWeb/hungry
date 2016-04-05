e0be3fb5154c291f218c9d1f71aa85c8c12b4ff5
JamxWeb <287721705@qq.com>
2016-04-05 12:03:37 +0800

    add hungry

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('kaifanla',['ng'])
+  .controller('startCtrl', function ($scope) {
+        console.log('start')
+    })
+  .controller('mainCtrl', function ($scope) {
+        console.log('main')
+    })
+  .controller('detailCtrl', function ($scope) {
+        console.log('detail')
+    })
